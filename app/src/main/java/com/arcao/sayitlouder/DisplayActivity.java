ddadd88e14ad916b98e4486eb68a29a7672d1c7b
Arcao <arcao@arcao.com>
2016-03-09 22:38:59 +0100

    Force set window background to selected color

--- app/src/main/java/com/arcao/sayitlouder/DisplayActivity.java
+++ app/src/main/java/com/arcao/sayitlouder/DisplayActivity.java
@@ -2,6 +2,7 @@ package com.arcao.sayitlouder;
 
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -28,6 +29,8 @@ public class DisplayActivity extends AppCompatActivity {
 		configuration.setShakeThresholdForce(getPrefsInt(prefs, "shakeThresholdForce", configuration.getShakeThresholdForce()));
 		configuration.setHighlightMode(getPrefsInt(prefs, "highlightMode", configuration.getHighlightMode()));
 		configuration.setMirroredY(prefs.getBoolean("mirroredY", configuration.isMirroredY()));
+
+		getWindow().setBackgroundDrawable(new ColorDrawable(configuration.getBackgroundColor()));
 
 		view = new MessageView(this, configuration, getIntent().getStringExtra(INTENT_EXTRA_MESSAGE));
 
